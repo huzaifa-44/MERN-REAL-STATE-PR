dd914bf9f466b2d962a1ec3478a085adef0020cb
Huzaifa Arshad <huzaifaarshad197@gmail.com>
2026-02-21 17:51:48 +0500

    Connect to database MongoDB

--- huzaifa_real_estate/api/index.js
+++ huzaifa_real_estate/api/index.js
@@ -1,8 +1,20 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to mongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 const port = 3000;
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
