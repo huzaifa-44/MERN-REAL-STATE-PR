831fdcc4234574b70b8a6947872cfe81896008f8
Huzaifa Arshad <huzaifaarshad197@gmail.com>
2026-02-23 17:06:56 +0500

    create a test api route

--- huzaifa_real_estate/api/index.js
+++ huzaifa_real_estate/api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
 dotenv.config();
 
 mongoose
@@ -18,3 +19,6 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+
+app.use("/api/user", userRouter);
--- huzaifa_real_estate/api/models/users.model..js
+++ huzaifa_real_estate/api/models/users.model..js
@@ -15,7 +15,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     unique: true,
   },
-})({ timestamps: true });
+}, { timestamps: true });
 
 const User = mongoose.model('User', userSchema);
 
