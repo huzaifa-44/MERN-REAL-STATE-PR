7beb3e2a9a258a74d10bad3aaf110a30e9658ee7
Huzaifa Arshad <huzaifaarshad197@gmail.com>
2026-03-13 17:25:34 +0500

    add redux persist

--- huzaifa_real_estate/frontend/src/redux/store.js
+++ huzaifa_real_estate/frontend/src/redux/store.js
@@ -1,10 +1,26 @@
-import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "../redux/User/userSlice";
+import { combineReducers, configureStore } from "@reduxjs/toolkit"; // 1. combineReducers add kiya
+import userReducer from "./User/userSlice";
+import { persistReducer, persistStore } from "redux-persist"; // 2. persistReducer (ed hata diya)
+import storage from "redux-persist/lib/storage"; // 3. Curly braces {} hata diye
+
+// Saare reducers ko combine karein
+const rootReducer = combineReducers({ user: userReducer });
+
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+
+// persistReducer function se persistedReducer variable banayein
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: { user: userReducer },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 });
+
+export const persistor = persistStore(store);
